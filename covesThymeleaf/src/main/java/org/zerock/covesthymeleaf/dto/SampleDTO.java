bd4a58b50be83e24de92a83aa9c9b25a68751159
Fekim <81150979+97Fekim@users.noreply.github.com>
2022-03-30 01:09:13 +0900

    thymeleaf

---
+++ covesThymeleaf/src/main/java/org/zerock/covesthymeleaf/dto/SampleDTO.java
@@ -0,0 +1,17 @@
+package org.zerock.covesthymeleaf.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@Builder(toBuilder = true)
+public class SampleDTO {
+
+    private Long sno;
+    private String first;
+    private String last;
+    private LocalDateTime regTime;
+}
